c0e500c9eb548235e341d4fa2ea27de180dbd217
Rani <109195584+ranisharma09@users.noreply.github.com>
2022-10-02 17:00:52 +0530

    Add files via upload

---
+++ R.py
@@ -0,0 +1,16 @@
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Rani64 import asad
+    kr()
+elif bit == '32bit':
+    from Rani32 import asad
+    kr()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
